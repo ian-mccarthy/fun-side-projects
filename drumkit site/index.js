e3b8aaa7f902d8a3da1818a86a47bf0c51d77f7d
ian-mccarthy <106149807+ian-mccarthy@users.noreply.github.com>
2022-11-04 16:54:05 +0000

    Add files via upload

---
+++ drumkit site/index.js
@@ -0,0 +1,60 @@
+let numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i<numberOfButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        
+        let buttonInnerHtml = this.innerHTML;
+
+        makeSound(buttonInnerHtml)
+
+        })
+}
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+});
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio ("sounds/tom-1.mp3");
+            tom1.play(); 
+            break;
+
+        case "a":
+            let tom2 = new Audio ("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio ("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            let tom4 = new Audio ("sounds/tom-4.mp3")
+            tom4.play();
+            break;
+
+        case "j":
+            let snare = new Audio ("sounds/kick-bass.mp3")
+            snare.play();
+            break;
+
+        case "k":
+            let crash = new Audio ("sounds/snare.mp3")
+            crash.play();
+            break;
+
+        case "l":
+            let kick = new Audio("sounds/crash.mp3")
+            kick.play();
+            break;
+
+        default:
+            console.log(buttonInnerHtml)
+            break;
+    }
+}
